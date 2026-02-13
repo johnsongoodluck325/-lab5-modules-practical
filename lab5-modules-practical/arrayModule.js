cb7db4049b4e614842050f8f6918440589095498
johnsongoodluck325 <johnsongoodluck325@gmail.com>
2026-02-12 21:09:13 -0500

    Delete lab5-modules-practical directory

--- lab5-modules-practical/arrayModule.js
+++
@@ -1,8 +0,0 @@
-
-export function findMax(arr) {
-    return Math.max(...arr);
-}
-
-export function reverseArray(arr) {
-    return [...arr].reverse();
-}
--- lab5-modules-practical/stringModule.js
+++
@@ -1,7 +0,0 @@
-export function toUpperCase(str) {
-    return str.toUpperCase();
-}
-
-export function toLowerCase(str) {
-    return str.toLowerCase();
-}
